234799664c7a2100779e1b4a6902c56a1444df38
Gabriel Barbera <gabrielbarbera@gmail.com>
2019-03-29 16:25:27 -0300

    Antes de agregar el search box

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor() {
+		super()
+		this.state = {
+			robots: robots,
+			searchField: ""
+		}
+	}
+
+	onSearchChange(event) {
+		console.log(event);
+	}
+
+	render(){
+		return (
+			<div className="tc"> 
+				<h1>Robo Friends</h1>
+				<SearchBox searchChange={this.onSearchChange()} />
+				<CardList robots={this.state.robots}/>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/robots.js
@@ -0,0 +1,61 @@
+export const robots = [{
+    "id": 1,
+    "name": "Carlynn Mawditt",
+    "username": "cmawditt0",
+    "email": "cmawditt0@topsy.com"
+}, {
+    "id": 2,
+    "name": "Ezmeralda Armatys",
+    "username": "earmatys1",
+    "email": "earmatys1@wikia.com"
+}, {
+    "id": 3,
+    "name": "Joletta Gilyott",
+    "username": "jgilyott2",
+    "email": "jgilyott2@archive.org"
+}, {
+    "id": 4,
+    "name": "Johann Mogie",
+    "username": "jmogie3",
+    "email": "jmogie3@cmu.edu"
+}, {
+    "id": 5,
+    "name": "Min Brasseur",
+    "username": "mbrasseur4",
+    "email": "mbrasseur4@sohu.com"
+}, {
+    "id": 6,
+    "name": "Garret Kruger",
+    "username": "gkruger5",
+    "email": "gkruger5@berkeley.edu"
+}, {
+    "id": 7,
+    "name": "Carlynn Passo",
+    "username": "cpasso6",
+    "email": "cpasso6@chronoengine.com"
+}, {
+    "id": 8,
+    "name": "Felike Bibbie",
+    "username": "fbibbie7",
+    "email": "fbibbie7@slate.com"
+}, {
+    "id": 9,
+    "name": "Raphael Wychard",
+    "username": "rwychard8",
+    "email": "rwychard8@tiny.cc"
+}, {
+    "id": 10,
+    "name": "Julita Miskin",
+    "username": "jmiskin9",
+    "email": "jmiskin9@t.co"
+}, {
+    "id": 11,
+    "name": "Margret Armstead",
+    "username": "marmsteada",
+    "email": "marmsteada@unesco.org"
+}, {
+    "id": 12,
+    "name": "Kahaleel Ladell",
+    "username": "kladellb",
+    "email": "kladellb@blogger.com"
+}]
